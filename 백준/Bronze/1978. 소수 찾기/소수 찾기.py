18def3bc4b6ffd50ebddff66a58d4c6dc8da8f65
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2024-03-21 21:04:08 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,15 @@
+n = int(input())
+nums = list(map(int, input().split()))
+count = 0;
+
+for num in nums:
+    if num == 1:
+        continue
+
+    for i in range(2, num):
+        if num%i == 0:
+            break
+    else:
+        count += 1
+
+print(count)

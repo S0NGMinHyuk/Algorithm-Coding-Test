b0c929039a41ee7125673e6b3a0a0691c09f238e
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2024-08-20 07:09:51 +0900

    [level 3] Title: 거스름돈, Time: 509.94 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.py
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -1,7 +1,6 @@
 # DP 알고리즘 사용
 def solution(n, money):
     dataBase = [0]*(n+1)    # index 원을 거슬러줄 수 있는 경우의 수
-    money.sort()            # 코인을 오름차순으로 정렬
 
     for coin in money:
         for i in range(coin, n+1):

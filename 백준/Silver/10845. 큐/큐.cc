e0590201be806ff930d7b89e7561edc90df196e5
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2025-03-27 15:36:36 +0900

    [Silver IV] Title: 큐, Time: 224 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+
+using namespace std;
+
+int main() {
+    int loop, input, result, isPrint = 0; 
+    queue<int> queue;   // 큐 변수
+    string cmd;         // 명령어 변수
+
+    cin >> loop;        // 반복횟수 입력받기
+    for (int i = 0; i < loop; i++) {
+        cin >> cmd;     // 명령어 입력받기
+        isPrint = 1;
+
+        // 명령어에 맞는 기능 수행
+        if (cmd == "push") {
+            isPrint = 0;    // push 명령어는 출력 X
+            cin >> input;
+            queue.push(input);
+        }
+        else if (cmd == "pop") {
+            if (queue.empty()) result = -1; // 빈 스택인 경우 -1 출력
+            else {
+                result = queue.front();       // c++의 pop 함수는 리턴이 없다.
+                queue.pop();
+            }
+        }
+        else if (cmd == "size") {
+            result = queue.size();
+        }
+        else if (cmd == "empty") {
+            result = queue.empty();
+        }
+        else if (cmd == "front") {
+            if (queue.empty()) result = -1; // 빈 스택인 경우 -1 출력
+            else result = queue.front();
+        }
+        else if (cmd == "back") {
+            if (queue.empty()) result = -1; // 빈 스택인 경우 -1 출력
+            else result = queue.back();
+        }
+
+        if (isPrint) cout << result << "\n"; // 결과 출력
+
+    }
+}

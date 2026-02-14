4ed67f7a1dc206e5c92ee9abb5e4c5037df72beb
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2026-02-15 00:18:57 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14364 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -12,14 +12,15 @@ public class Main {
     String key = br.readLine();
 
     long r = 1L;
-    int M = 1234567891;
-    int result = 0;
+    long M = 1234567891;
+    long result = 0;
 
     for (int i=0; i<N; i++) {
       int value = key.charAt(i) - 'a' + 1;
       result += value * r % M;
       r *= 31;
+      r %= M;
     }
-    System.out.println(result);
+    System.out.println(result % M);
   }
 }
